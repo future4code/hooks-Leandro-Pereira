4af42fe910ba1b571f248ee651de9c955f1137e6
Leandro Pereira de Jesus <Le0406@hotmail.com>
2022-04-07 22:04:22 -0300

    2

--- Modulo 2/labetube1/index.js
+++ Modulo 2/labetube1/index.js
@@ -1,3 +1,4 @@
+//Exercicio 01
 // function criarArrayNomesAnimais() {
 //     const animais = [
 //       { nome: "Cachorro", classificacao: "mamífero" },
@@ -11,28 +12,23 @@
 // }
 // console.log(criarArrayNomesAnimais)
 
-function filtraTarefas(){
-  const tarefas = [
-    { titulo: "Fazer Remember", status: "done", tempo: 30 },
-    { titulo: "Fazer Challange", status: "todo", tempo: 30 },
-    { titulo: "Assistir Aula", status: "done", tempo: 120 },
-    { titulo: "Fazer almoço nutritivo", status: "done", tempo: 60 },
-    { titulo: "Ler atentamente exercícios do dia", status: "doing", tempo: 20 },
-    { titulo: "Fazer exercício do dia", status: "doing", tempo: 180 },
-    { titulo: "Fazer desafio", status: "doing", tempo: 60 },
-    { titulo: "Ir para o Stand Up", status: "todo", tempo: 30 },
-    { titulo: "Enviar Feedback", status: "todo", tempo: 15 },
-    { titulo: "Enviar Exercício", status: "todo", tempo: 15 },
-    { titulo: "Jogar Videogame", status: "todo", tempo: 120 },
-    { titulo: "Assistir Novela", status: "todo", tempo: 90 }
-  ]
-   const tarefasTerminadas = tarefas.filter(item=>{
-     return item.status === "done"
-   })
-   
-   const nomes = tarefasTerminadas.map(item=>{
-     return item.titulo
-   })
-  
-}
-console.log(filtraTarefas)
+//Exercício 02
+// function filtraTarefas() {
+//   const tarefas = [
+//     { titulo: "Fazer Remember", status: "done", tempo: 30 },
+//     { titulo: "Fazer Challange", status: "todo", tempo: 30 },
+//     { titulo: "Assistir Aula", status: "done", tempo: 120 },
+//     { titulo: "Fazer almoço nutritivo", status: "done", tempo: 60 },
+//     { titulo: "Ler atentamente exercícios do dia", status: "doing", tempo: 20 },
+//     { titulo: "Fazer exercício do dia", status: "doing", tempo: 180 },
+//     { titulo: "Fazer desafio", status: "doing", tempo: 60 },
+//     { titulo: "Ir para o Stand Up", status: "todo", tempo: 30 },
+//     { titulo: "Enviar Feedback", status: "todo", tempo: 15 },
+//     { titulo: "Enviar Exercício", status: "todo", tempo: 15 },
+//     { titulo: "Jogar Videogame", status: "todo", tempo: 120 },
+//     { titulo: "Assistir Novela", status: "todo", tempo: 90 }
+//  ]
+
+// const novoArray = (tarefas.filter((({status})=>status === "done")).map(({titulo})=>titulo))
+// return novoArray
+// }
