9c360b1cbcb343d45345a5fb310797f8d4259f96
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-04-22 18:45:00 -0300

    exercício react2

--- Modulo 3/props-labedin/src/components/CardGrande/CardGrande.js
+++ Modulo 3/props-labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,39 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from 'styled-components'
+
+const CardGrande1 = styled.main`
+  display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 200px;
+`
+
+const CardGrande2 = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+const CardGrande3 = styled.h4`
+margin-bottom: 15px;
+`
+
+const CardGrande4 = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`
 
 function CardGrande(props) {
     return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
+        <CardGrande1 className="bigcard-container">
+            <CardGrande2     src={ props.imagem } />
+            <CardGrande4>
+            <CardGrande3>{ props.nome }</CardGrande3>
                 <p>{ props.descricao }</p>
-            </div>
-        </div>
+            </CardGrande4>
+        </CardGrande1>
     )
 }
-
 export default CardGrande;
--- Modulo 3/props-labedin/src/components/CardPequeno/CardPequeno.js
+++ Modulo 3/props-labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import'./CardPequeno.css'
+import styled from 'styled-components'
+
+
+const CardPequeno2 = styled.main`
+   display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 30px 20px;
+    margin-bottom: 10px;
+    width: 550px;
+`
+const CardPequeno3 = styled.img`
+    width: 50px;
+    height: 50px;
+    padding: 10px 10px;
+`
 
 function CardPequeno(props){
     return(
-        <div className="small-container">
-            <img src={props.imagem}/>
+        <CardPequeno2 className="small-container">
+            <CardPequeno3 src={props.img}/>
             <div>
                 <p>{ props.email }</p>
                 <p>{ props.endereco }</p>
             </div>
-        </div>
+        </CardPequeno2>
     )
 }
 
