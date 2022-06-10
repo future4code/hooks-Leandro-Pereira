31780a514e0a90a080d42539e1578e0a39efe28b
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-06-09 23:44:23 -0300

    todos os requisitos funcionais

---
+++ Modulo 4/astromatch/src/components/Profile/Styles.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    display:flex;
+    justify-content:center;
+    flex-direction:column;
+    margin:0 auto;
+    position: relative;
+    border:1px solid black;
+    width:40%;
+    align-items: center;
+    background-color: white;
+    height:100vh;
+`
+
+export const Title = styled.h1`
+    display:flex;
+    justify-content:center;
+
+`
+
+export const Button = styled.button`
+display:flex;
+justify-content:center;
+width:50%;
+`
+export const Img = styled.img`
+display:flex;
+width:500px;
+height:470px;
+`
+
+
+
