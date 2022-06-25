e44d284eef19e5cb7dc13d08ee47bd6b0d5f8ea5
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-06-24 21:29:32 -0300

    incompleto

--- Modulo 4/projeto-labex/src/Pages/CreateTripPage/CreateTripPage.js
+++ Modulo 4/projeto-labex/src/Pages/CreateTripPage/CreateTripPage.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import {Tittle, FormContainer, InputItem, Button} from './styles'
 import Headers from '../Header/Headers';
-
+import axios from 'axios'
 
 
 const CreateTripPage = () =>{
+
+    const createTrip = () =>{
+        const headers = {
+            auth: localStorage.getItem('Token')
+        }
+
+        const body = {
+            name : '',
+            planet: '',
+            date: '',
+            description: '',
+            duration: 0
+        }
+
+        axios.post(`https://us-central1-labenu-apis.cloudfunctions.net/labeX/leandro-pereira-hooks/trips`,body,headers)
+        .then((response) => {
+            alert('Your trip was created successfully')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
+
     return(
         <div>
             <Headers/>
@@ -15,8 +39,9 @@ const CreateTripPage = () =>{
                <InputItem placeholder="Date" type="date"/>
                <InputItem placeholder="Description" type="text"/>
                <InputItem placeholder="Duration in days" type="number"/>
-               <Button type="submit">Send</Button>
+               <Button onClick={()=>createTrip()} type="submit">Send</Button>
            </FormContainer>
+           
         </div>
     )
 }
