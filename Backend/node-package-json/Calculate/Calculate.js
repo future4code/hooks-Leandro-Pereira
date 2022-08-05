d08b13e33a713638ff863dc11b93ca99db8c9627
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-08-04 21:32:43 -0300

    node-package-json

---
+++ Backend/node-package-json/Calculate/Calculate.js
@@ -0,0 +1,14 @@
+const num_1 = Number(process.argv[2])
+const num_2 = Number(process.argv[3])
+
+const result = num_1 + num_2
+console.log(`Resposta: ${result}`);
+
+const result_1 = num_1 - num_2
+console.log(`Resposta: ${result_1}`);
+
+const result_2 = num_1 * num_2
+console.log(`Resposta: ${result_2}`);
+
+const result_3 = num_1 / num_2
+console.log(`Resposta: ${result_3}`);
