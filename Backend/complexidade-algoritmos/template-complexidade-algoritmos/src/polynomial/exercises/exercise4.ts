adf43f3280de7165433ca87df6755fa7b26ac7d3
Leandro Pereira de Jesus <le0406@hotmail.com>
2022-12-01 19:06:26 -0300

    exercise complexity-algorithm

---
+++ Backend/complexidade-algoritmos/template-complexidade-algoritmos/src/polynomial/exercises/exercise4.ts
@@ -0,0 +1,11 @@
+function verifyIfExistRepeatedNumbers(listOfNumbers: number[]): boolean {
+  for (let i = 0; i < listOfNumbers.length; i++) {
+    if (listOfNumbers.indexOf(listOfNumbers[i]) !== i) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+// O(n * n) n ao quadrado
---
+++ Backend/complexidade-algoritmos/template-complexidade-algoritmos/src/polynomial/exercises/exercise5.ts
@@ -0,0 +1,2 @@
+// Da maior eficiência/performance para a menor
+// 3 → 1 = 2 → 4
